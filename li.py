ae37ef40801a2a5e4003b9997a326d9c3d66125f
ashahampannavar73-cell <ashahampannavar73@gmail.com>
2025-09-27 12:13:47 +0530

    Add files via upload

---
+++ li.py
@@ -0,0 +1,105 @@
+"""flowers=["Rose",50,"Red"]
+print(flowers[0])
+print(flowers[-1])
+print(flowers[2])
+
+flowers=["Rose",50,"Red"]
+print(flowers)
+flowers.remove(50)
+for x in flowers:
+ print(x)
+amount=[25,20,50]
+amount.remove(flowers)
+for a in amount:
+ print(a)
+
+flowers=["Rose",50,"Red"]
+print(flowers)
+flowers.reverse()
+for x in flowers:
+ print(x)
+flowers=["Rose",50,"Red"]
+print(len(flowers))
+
+flowers=["Rose",50,"Red"]
+print(type(flowers))
+
+flowers=["Rose",50,"Red"]
+flowers.insert(2,"daisy")
+flowers.insert(3,"marigold")
+print(flowers)
+
+flowers=["Rose",50,"Red"]
+m = flowers.copy()
+print(m)
+
+print("joining method")
+flowers=["Rose","bluestar","daisy"]
+color=["red","purple","white"]
+Newlist=flowers+color
+print(Newlist)
+
+flowers=["Rose","bluestar","daisy"]
+flowers.remove("Rose")
+print(flowers)
+ 
+Names=["doreamon","hathodi","dora","bean"]
+print(Names[0:4])
+
+Names=["doreamon","hathodi","dora","bean"]
+del Names[1]
+print(Names)
+ 
+Names=("doreamon","hathodi","dora","bean")
+Names.sort()
+for i in Names:
+ print(i)"""
+Names=["doreamon","hathodi","dora","bean"]
+Names.pop(1)
+print(Names)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
